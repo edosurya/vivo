d7744aecd431428f8684853f1bd7d95b3dc02c21
Surya <edo.surya@mcsaatchi.com>
2024-09-14 22:44:41 +0700

    width 50% upload files

--- resources/views/frontend/register.blade.php
+++ resources/views/frontend/register.blade.php
@@ -165,7 +165,7 @@
                             <div class="form-group mb-3">
                                 <div class="mb-3"><h6 class="text-white vivo_light"><i>Upload photo max 20Mb* </i></h6></div>
                                 <div class="main-drag-area form-control p-0 border-0" id="dzDropzone">
-                                    <div class="dz-message rounded-3 text-muted bg-blue-gradient-90 p-3 mb-2" id="dzPlaceholder" style="cursor: pointer;">
+                                    <div class="dz-message rounded-3 text-muted bg-blue-gradient-90 p-3 mb-2 w-50" id="dzPlaceholder" style="cursor: pointer;">
                                         <span class="text-black vivo_bold">Upload Photo</span>
                                         <svg class="dz-photo-icon opacity-75" xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/>
                                             <path d="M15 8h.01" /><path d="M3 6a3 3 0 0 1 3 -3h12a3 3 0 0 1 3 3v12a3 3 0 0 1 -3 3h-12a3 3 0 0 1 -3 -3v-12z" /><path d="M3 16l5 -5c.928 -.893 2.072 -.893 3 0l5 5" /><path d="M14 14l1 -1c.928 -.893 2.072 -.893 3 0l3 3" />
@@ -212,7 +212,7 @@
         </li>
     </script>    
     <script id="dzAdditionalTemplate" type="text/template">
-        <div class="dz-additional-area text-muted position-relative form-control d-flex bg-blue-gradient-90 mb-2 rounded-2 border-0 p-3" style="cursor: pointer;">
+        <div class="dz-additional-area text-muted position-relative form-control d-flex bg-blue-gradient-90 mb-2 rounded-2 border-0 p-3 w-50" style="cursor: pointer;">
             <span class="text-black vivo_bold">Upload Photo</span>
             <svg class="dz-photo-icon opacity-75" xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/>
                 <path d="M15 8h.01" /><path d="M3 6a3 3 0 0 1 3 -3h12a3 3 0 0 1 3 3v12a3 3 0 0 1 -3 3h-12a3 3 0 0 1 -3 -3v-12z" /><path d="M3 16l5 -5c.928 -.893 2.072 -.893 3 0l5 5" /><path d="M14 14l1 -1c.928 -.893 2.072 -.893 3 0l3 3" />
