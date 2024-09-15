c5211c8f9ca1eb8b904fddd850c419757793574f
Surya <edo.surya@mcsaatchi.com>
2024-09-15 16:03:20 +0700

    back to before css setting gallery

--- resources/views/frontend/gallery.blade.php
+++ resources/views/frontend/gallery.blade.php
@@ -143,7 +143,7 @@
                             @if($key == 0)
                             <div class="carousel-item active">
                                 <img src="{{ asset($img['path'])}}" loading="lazy">
-                                <div class="text pt-3 text-center">
+                                <div class="text pt-3 w-100 text-center">
                                     <p class="text-white vivo_bold text-uppercase mb-1 fs-img-title">{{ $img['title'] }}</p>
                                     <p class="text-white vivo_regular fs-img-dec mb-n1">{{ $img['desc'] }}</p>
                                     <p class="text-white vivo_regular fs-img-dec">{{ $img['location'] }}</p>
@@ -152,7 +152,7 @@
                             @else
                             <div class="carousel-item">
                                 <img src="{{ asset($img['path'])}}" class="d-block w-100"  loading="lazy">
-                                <div class="text pt-3 text-center">
+                                <div class="text pt-3 w-100 text-center">
                                     <p class="text-white vivo_bold text-uppercase mb-1 fs-img-title">{{ $img['title'] }}</p>
                                     <p class="text-white vivo_regular fs-img-dec mb-n1">{{ $img['desc'] }}</p>
                                     <p class="text-white vivo_regular fs-img-dec">{{ $img['location'] }}</p>
