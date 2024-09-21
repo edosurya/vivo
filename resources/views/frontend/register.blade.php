3ffeb0eec0570697375cc34d03a952dd8c4be5cb
Surya <edo.surya@mcsaatchi.com>
2024-09-21 22:19:05 +0700

    register textarea placeholder

--- resources/views/frontend/register.blade.php
+++ resources/views/frontend/register.blade.php
@@ -188,12 +188,8 @@
                                 Judul dan Deskripsi
                                 <span class="text-danger">*</span>
                             </label>
-                            <div style="font-size:12px; line-height: 14px;" class="mb-2">Tulis <b>judul dan deskripsi</b> untuk setiap foto yang diunggah di kolom dibawah ini. Judul maksimal 50 karakter dan deskripsi maksimal 250 karakter untuk setiap foto.</div>
-                            <textarea class="form-control indosat_body" name="img_desc" id="img_desc" rows="10" cols="50" maxlength="1800">Contoh:
-Foto 1
-Judul: Negeri di Atas Awan
-Deskripsi: Pegunungan menjulang di atas lautan awan putih, memancarkan ketenangan dan kebahagiaan. Langit biru cerah, sinar mentari hangat menyinari, menciptakan harmoni alam yang mengundang rasa syukur dan sukacita dalam diri.</textarea>
-                            <label for="counter-input" class="label">Karakter <span id="counter-display" class="tag is-success">0</span>/1800
+                            <textarea class="form-control indosat_body" name="img_desc" id="img_desc" rows="10" cols="50" maxlength="1600" placeholder="Tulis judul dan deskripsi untuk setiap foto yang diunggah di kolom ini.&#10;Judul maksimal 50 karakter dan deskripsi maksimal 250 karakter untuk setiap foto.&#10;Contoh:&#10;Foto 1&#10;Judul: Negeri di Atas Awan&#10;Deskripsi: Pegunungan menjulang di atas lautan awan putih, memancarkan ketenangan dan kebahagiaan. Langit biru cerah, sinar mentari hangat menyinari, menciptakan harmoni alam yang mengundang rasa syukur dan sukacita dalam diri."></textarea>
+                            <label for="counter-input" class="label">Karakter <span id="counter-display" class="tag is-success">0</span>/1600
                             <label class="invalid-feedback fw-bold mb-3" id="imgDescErrorMessage"></label>
                         </div>
 
