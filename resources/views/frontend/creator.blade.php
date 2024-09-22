9fb3f14bfde539b79b0d2020fc5c52698bddf3dd
Surya <edo.surya@mcsaatchi.com>
2024-09-22 21:37:24 +0700

    fixed creator page

--- resources/views/frontend/creator.blade.php
+++ resources/views/frontend/creator.blade.php
@@ -1,121 +1,22 @@
 @extends('layouts.frontend')
 
-@section('title', 'Galeri')
+@section('title', 'Creatot Details')
 @section('meta_title', 'vivo IMAGINE')
 @section('meta_description','vivo IMAGINE')
 
     @push('css-plugin')
       <link href="{{ asset('frontend/css/homepage.css') }}" rel="stylesheet" type="text/css" id="bootstrap">
-      <link rel="stylesheet" href="{{ asset('frontend/css/owl.carousel.min.css') }} ">
-      <link rel="stylesheet" href="{{ asset('frontend/css/owl.theme.default.min.css') }}">
-      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/4.5.6/css/ionicons.min.css">
       <link rel="stylesheet" href="{{ asset('frontend/css/gallery.css') }}">
     @endpush
 
     @push('style')
     <style type="text/css">
-
-
-.gallery-item {
-  display: block;
-}
-
-.gallery-item img {
-  box-shadow: 0 1rem 1rem rgba(0, 0, 0, 0.15);
-  transition: box-shadow 0.2s;
-}
-
-.gallery-item:hover img {
-  box-shadow: 0 1rem 1rem rgba(0, 0, 0, 0.35);
-}
-
-.lightbox-modal .modal-content {
-  background-color: var(--lightbox);
-}
-
-.lightbox-modal .btn-close {
-  position: absolute;
-  top: 1.25rem;
-  right: 1.25rem;
-  font-size: 1.25rem;
-  z-index: 10;
-  filter: invert(1) grayscale(100);
-}
-
-.lightbox-modal .modal-body {
-  display: flex;
-  align-items: center;
-  padding: 0;
-}
-
-.lightbox-modal .lightbox-content {
-  width: 100%;
-}
-
-.lightbox-modal .carousel-indicators {
-  margin-bottom: 0;
-}
-
-.lightbox-modal .carousel-indicators [data-bs-target] {
-  background-color: var(--carousel-text) !important;
-}
-
-.lightbox-modal .carousel-inner {
-  width: 75%;
-}
-
-.lightbox-modal .carousel-inner img {
-  animation: zoomin 10s linear infinite;
-}
-
-.lightbox-modal .carousel-item .carousel-caption {
-  right: 0;
-  bottom: 0;
-  left: 0;
-  padding-bottom: 2rem;
-  background-color: var(--lightbox);
-  color: var(--carousel-text) !important;
-}
-
-.lightbox-modal .carousel-control-prev,
-.lightbox-modal .carousel-control-next {
-  width: auto;
-}
-
-.lightbox-modal .carousel-control-prev {
-  left: 1.25rem;
-}
-
-.lightbox-modal .carousel-control-next {
-  right: 1.25rem;
-}
-
-@media (min-width: 1400px) {
-  .lightbox-modal .carousel-inner {
-    max-width: 60%;
-  }
-}
-
-[data-bs-theme = "dark"] .lightbox-modal .carousel-control-next-icon,
-[data-bs-theme = "dark"] .lightbox-modal .carousel-control-prev-icon {
-    filter: none;
-}
-
-
-.bi {
-  display: inline-block;
-  width: 1em;
-  height: 1em;
-  vertical-align: -0.035em;
-  fill: currentcolor;
-}
-
-.btn-download {
-    font-size: 15px;
-    border-color: none;
-    background: rgb(42,144,190);
-    background: linear-gradient(90deg, rgba(42,144,190,1) 0%, rgba(114,216,221,1) 49%, rgba(42,144,190,1) 100%);
-}
+      .btn-download {
+          font-size: 15px;
+          border-color: none;
+          background: rgb(42,144,190);
+          background: linear-gradient(90deg, rgba(42,144,190,1) 0%, rgba(114,216,221,1) 49%, rgba(42,144,190,1) 100%);
+      }
 
     </style>
     @endpush
@@ -124,39 +25,45 @@
 
       <section class="pb-6 bg-black mt-4">
         <div class="container-lg mb-2">
-          <div class="row" data-aos="fade-down" data-aos-duration="1500">
-            <div class="col-10 col-lg-12">
+          <div class="row mb-4" data-aos="fade-down" data-aos-duration="1500">
+            <div class="col-12 col-lg-12">
               <h3 class="text-white vivo_heavy text-uppercase">{{ $creator->fullname }}</h3>
-              <p class="text-white">{{ $creator::IMAGE_CATEGORY_2[$creator->category] }}</p>
+              <h5 class="text-white">Kategori - {{ $creator::IMAGE_CATEGORY_2[$creator->category] }}</h5>
+              <h5 class="text-white">Deskripsi Foto:</h5>
               <p class="text-white">{!! nl2br($creator->desc) !!}</p>
-              <a href="{{ route('creator.download', $creator->code ) }}" class="btn rounded-3 text-black vivo_heavy btn-download text-btn-padding" id="dzSubmitButton">Download Foto</a>
             </div>
           </div>
 
-
-          <section class="photo-gallery">
+          <div class="photo-gallery">
             <div class="container">
-              <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4 gallery-grid">
+              <div class="mb-4 d-flex justify-content-end">
+              <a href="{{ route('creator.download', $creator->code ) }}" class="btn rounded px-5 py-3 text-black vivo_heavy btn-download text-btn-padding" id="dzSubmitButton">Download Foto</a>
+            </div>
+              <div class="row row-cols-1 row-cols-md-2 row-cols-lg-4 g-2 gallery-grid">
                 @foreach($creator->relatedImages as $img)
                 <div class="col">
                   <a class="gallery-item" href="{{ asset('storage/'.$img->path) }}">
-                    <img src="{{ asset('storage/'.$img->path) }}" class="img-fluid">
+                    <img src="{{ asset('storage/'.$img->path) }}" class="img-fluid rounded">
                   </a>
                 </div>
                 @endforeach
               </div>
             </div>
-          </section>
+          </div>
+
+
+          <div class="row" data-aos="fade-down" data-aos-duration="1500">
+            <div class="col-10 col-lg-12">
+            </div>
+          </div>
+
       </div>
 </section>
 
-<div class="modal fade lightbox-modal" id="lightbox-modal" tabindex="-1">
+<div class="modal fade lightbox-modal" id="lightbox-modal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <span class="close" data-bs-dismiss="modal" aria-label="Close">&times;</span>
   <div class="modal-dialog modal-dialog-centered modal-fullscreen">
     <div class="modal-content">
-      <button type="button" class="btn-fullscreen-exit d-none" aria-label="Exit fullscreen">
-        <svg class="bi"><use href="#exit"></use></svg>
-      </button>
-      <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       <div class="modal-body">
         <div class="lightbox-content">
           <!-- JS content here -->
@@ -166,12 +73,13 @@
   </div>
 </div>
 
+
 @endsection
 
 @push('js-plugin')
 
 <script type="text/javascript">
-  const html = document.querySelector('html');
+const html = document.querySelector('html');
 html.setAttribute('data-bs-theme', 'dark');
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -182,31 +90,6 @@ document.addEventListener('DOMContentLoaded', () => {
   const lightboxModal = document.getElementById("lightbox-modal");
   const bsModal = new bootstrap.Modal(lightboxModal);
   const modalBody = lightboxModal.querySelector(".lightbox-content");
-
-  function createCaption (caption) {
-    return `<div class="carousel-caption d-none d-md-block">
-        <h4 class="m-0">${caption}</h4>
-      </div>`;
-  }
-
-  function createIndicators (img) {
-    let markup = "", i, len;
-
-    const countSlides = links.length;
-    const parentCol = img.closest('.col');
-    const curIndex = [...parentCol.parentElement.children].indexOf(parentCol);
-
-    for (i = 0, len = countSlides; i < len; i++) {
-      markup += `
-        <button type="button" data-bs-target="#lightboxCarousel"
-          data-bs-slide-to="${i}"
-          ${i === curIndex ? 'class="active" aria-current="true"' : ''}
-          aria-label="Slide ${i + 1}">
-        </button>`;
-    }
-
-    return markup;
-  }
 
   function createSlides (img) {
     let markup = "";
@@ -228,11 +111,7 @@ document.addEventListener('DOMContentLoaded', () => {
   function createCarousel (img) {
     const markup = `
       <!-- Lightbox Carousel -->
-      <div id="lightboxCarousel" class="carousel slide carousel-fade" data-bs-ride="true">
-        <!-- Indicators/dots -->
-        <div class="carousel-indicators">
-          ${createIndicators(img)}
-        </div>
+      <div id="lightboxCarousel" class="carousel slide" data-bs-ride="true">
         <!-- Wrapper for Slides -->
         <div class="carousel-inner justify-content-center mx-auto">
           ${createSlides(img)}
@@ -272,28 +151,8 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   }
 
-  // --- Support Fullscreen
-  const fsEnlarge = document.querySelector(".btn-fullscreen-enlarge");
-  const fsExit = document.querySelector(".btn-fullscreen-exit");
 
-  function enterFS () {
-    lightboxModal.requestFullscreen().then({}).catch(err => {
-      alert(`Error attempting to enable full-screen mode: ${err.message} (${err.name})`);
-    });
-    fsEnlarge.classList.toggle("d-none");
-    fsExit.classList.toggle("d-none");
-  }
 
-  function exitFS () {
-    document.exitFullscreen();
-    fsExit.classList.toggle("d-none");
-    fsEnlarge.classList.toggle("d-none");
-  }
-
-  fsExit.addEventListener("click", (e) => {
-    e.preventDefault();
-    exitFS();
-  });
 })
 
 
