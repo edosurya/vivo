806b5acaf0ff5588815f4656a49c1544bd4f181d
Surya <edo.surya@mcsaatchi.com>
2024-12-27 00:05:44 +0700

    reactive register page and disable submit btn

--- resources/views/frontend/register.blade.php
+++ resources/views/frontend/register.blade.php
@@ -247,7 +247,7 @@
                         </div>
 
                         <div class="mb-3 col-md-12 mb-0 pb-0 d-grid">
-                            <button class="btn rounded-3 text-black vivo_heavy btn-register text-btn-padding text-btn-padding" id="dzSubmitButton">SUBMIT</button>
+                            <button class="btn rounded-3 text-black vivo_heavy btn-register text-btn-padding text-btn-padding" id="dzSubmitButton" disabled>SUBMIT</button>
                         </div>
                     </div>
 
