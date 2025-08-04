f155fbd22b672c372949ac0b75fc74505e8fac79
Surya <edo.surya@mcsaatchi.com>
2025-08-04 17:13:26 +0700

    cat img bg

--- resources/views/frontend/homepage.blade.php
+++ resources/views/frontend/homepage.blade.php
@@ -9,7 +9,7 @@ $categories = [
   ['slug' => 'portrait-photography', 'label' => 'Portrait <br>Photography', 'image' => 'bg-cat-portrait.webp'],
   ['slug' => 'nature-architecture-photography', 'label' => 'Nature & Architecture <br>Photography', 'image' => 'bg-cat-nature.webp'],
   ['slug' => 'night-photography', 'label' => 'Night <br>Photography', 'image' => 'bg-cat-night.webp'],
-  ['slug' => 'street-life-photography', 'label' => 'Street <br>Photography', 'image' => 'bg-cat-street.webp'],
+  ['slug' => 'street-life-photography', 'label' => 'Street <br>Photography', 'image' => 'bg-cat-street2.webp'],
 ];
 
 
