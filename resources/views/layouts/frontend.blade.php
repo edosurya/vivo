ce2cbf10e17b8d61e1241047a0dcaba4bd3e8dbf
Surya <edo.surya@mcsaatchi.com>
2025-08-05 11:07:10 +0700

    change gtm

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -52,22 +52,13 @@
     @stack('style') {{-- Css Code --}}
 
     @if(env('APP_ENV') == 'production')
-    <!-- Google tag (gtag.js) -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-HPGN7RK5DS"></script>
-    <script>
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-
-      gtag('config', 'G-HPGN7RK5DS');
-    </script>
 
     <!-- Google Tag Manager -->
     <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
     new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
     j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
     'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer','GTM-W8SL2HV2');</script>
+    })(window,document,'script','dataLayer','GTM-5CVDM8FD');</script>
     <!-- End Google Tag Manager -->
 
     @endif
@@ -76,9 +67,10 @@
 <body>
     @if(env('APP_ENV') == 'production')
     <!-- Google Tag Manager (noscript) -->
-    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-W8SL2HV2"
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5CVDM8FD"
     height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
     <!-- End Google Tag Manager (noscript) -->
+
     @endif
 
     <!-- ===============================================-->
